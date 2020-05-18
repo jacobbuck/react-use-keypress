353fb8975e22368e3731c38c05004af21fa493eb
Jacob Buck <buck.jacob@gmail.com>
2020-05-18 20:01:53 +1200

    Add configuration files
    For babel, eslint, lint-staged, prettier and rollup

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'lib/index.cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'lib/index.esm.js',
+      format: 'esm',
+    },
+  ],
+  external: ['prop-types', 'react', 'react-dom'],
+  plugins: [babel()],
+};
