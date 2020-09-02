6bd0106bd1451017d522720b57668561dc9f68db
Jacob Buck <buck.jacob@gmail.com>
2020-09-02 07:26:05 +0000

    Set output.exports config in rollup to supress warning

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from '@rollup/plugin-babel';
 export default {
   input: 'src/index.js',
   output: [
-    { file: 'lib/index.cjs.js', format: 'cjs' },
+    { file: 'lib/index.cjs.js', format: 'cjs', exports: 'default' },
     { file: 'lib/index.esm.js', format: 'esm' },
   ],
   external: ['react', 'use-latest'],
