ffe83684c8a45631d96b92aadb79628ea344b3b6
Jacob Buck <buck.jacob@gmail.com>
2020-10-18 07:25:25 +0000

    Add type-checking (tests to come)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import useLatest from 'use-latest';
 import shimKeyboardEvent from './shimKeyboardEvent';
 
 const useKeypress = (keys, handler) => {
+  if (process.env.NODE_ENV !== 'production') {
+    if (Array.isArray(keys)) {
+      keys.forEach((key, i) => {
+        if (typeof key !== 'string') {
+          throw new TypeError(
+            `Expected \`keys[${i}]\` to be of type \`string\`, but received type \`${typeof key}\``
+          );
+        }
+      });
+    } else if (typeof keys !== 'string' && handler != null) {
+      throw new TypeError(
+        `Expected \`keys\` to be of type \`array\` or \`string\`, but received type \`${typeof handler}\``
+      );
+    }
+    if (typeof handler !== 'function' && handler != null) {
+      throw new TypeError(
+        `Expected \`handler\` to be of type \`function\`, but received type \`${typeof handler}\``
+      );
+    }
+  }
+
   const keysRef = useLatest(keys);
   const handerRef = useLatest(handler);
 
